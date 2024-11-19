1933fa916b455ef168e16332b48c6d26c7a554ef
Dhruvil Deepak Sheth <dhruvilsheth@proton.me>
2024-11-19 16:13:34 +0530

    made some chnages to backend

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}));
+app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,6 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
 		origin: '*',
-		credentials: true,
 		methods: ["GET", "POST"]
 	},
 });
